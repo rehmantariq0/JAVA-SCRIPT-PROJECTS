3a1d7927f980b34ebc67536490f70fcc24a3e0fd
rehmantariq0 <chaudharyrehman4343@gmail.com>
2025-02-28 22:18:44 +0500

    updates

--- firstwebdevelop.js
+++ firstwebdevelop.js
@@ -1,2 +1,44 @@
 console.log("First web Develop");
 first_name = "Abul Rehman";
+age = 24;
+num1 = 20;
+num2 = 30;
+// res = num1+num2;
+console.log(first_name);
+console.log(age);
+console.log(num1+num2);
+// "camael case" which mean that if we assigne a variable of 2 word start of first letter small of first letter and senced word start will be capital letter
+// fullName = "Jooon"3 type of keyword of diclaration let var and const.
+const pi= 3.14;
+let radius = 33;
+console.log(2*pi*radius);
+// these are the primitive data type
+let a = 10;
+let follow = true;
+let unfollow = false;
+let x;
+let y= null;
+console.log(typeof a);
+console.log(typeof unfollow);
+console.log(typeof x);
+console.log(typeof y);
+let k= BigInt(12345);
+console.log(typeof k);
+// lets make an Object
+const Student = {
+    StudentName : "Rehman Tariq",
+    StudentAge : 23,
+    StudentCGPA : 3.4,
+    StudentRegistration_No : "2023-ag-7850",
+};
+console.log(Student);
+console.log(Student.StudentAge);
+console.log(Student.StudentCGPA);
+const profile = {
+    username: "jon12",
+    fullName: "Jone Don",
+    Bio: "I am a Developer.",
+    Posts:33,
+    follower:199,   
+    following:5,   
+};
