a3aa5187994b9889e4b71e38f6ad06682d1f9575
rehmantariq0 <chaudharyrehman4343@gmail.com>
2025-03-03 20:23:54 +0500

    update

--- firstwebdevelop.js
+++ firstwebdevelop.js
@@ -1,67 +1,113 @@
-console.log("First web Develop");
-first_name = "Abul Rehman";
-age = 24;
-num1 = 20;
-num2 = 30;
+// console.log("First web Develop");
+// first_name = "Abul Rehman";
+// age = 24;
+// num1 = 20;
+// num2 = 30;
 // res = num1+num2;
-console.log(first_name);
-console.log(age);
-console.log(num1+num2);
+// console.log(first_name);
+// console.log(age);
+// console.log(num1+num2);
 // "camael case" which mean that if we assigne a variable of 2 word start of first letter small of first letter and senced word start will be capital letter
 // fullName = "Jooon"3 type of keyword of diclaration let var and const.
-const pi= 3.14;
-let radius = 33;
-console.log(2*pi*radius);
+// const pi= 3.14;
+// let radius = 33;
+// console.log(2*pi*radius);
 // these are the primitive data type
-let a = 10;
-let follow = true;
-let unfollow = false;
-let x;
-let y= null;
-console.log(typeof a);
-console.log(typeof unfollow);
-console.log(typeof x);
-console.log(typeof y);
-let k= BigInt(12345);
-console.log(typeof k);
+// let a = 10;
+// let follow = true;
+// let unfollow = false;
+// let x;
+// let y= null;
+// console.log(typeof a);
+// console.log(typeof unfollow);
+// console.log(typeof x);
+// console.log(typeof y);
+// let k= BigInt(12345);
+// console.log(typeof k);
 // lets make an Object
-const Student = {
-    StudentName : "Rehman Tariq",
-    StudentAge : 23,
-    StudentCGPA : 3.4,
-    StudentRegistration_No : "2023-ag-7850",
-};
-console.log(Student);
-console.log(Student.StudentAge);
-console.log(Student.StudentCGPA);
-const profile = {
-    username: "jon12",
-    fullName: "Jone Don",
-    Bio: "I am a Developer.",
-    Posts:33,
-    follower:199,   
-    following:5,   
-};
+// const Student = {
+//     StudentName : "Rehman Tariq",
+//     StudentAge : 23,
+//     StudentCGPA : 3.4,
+//     StudentRegistration_No : "2023-ag-7850",
+// };
+// console.log(Student);
+// console.log(Student.StudentAge);
+// console.log(Student.StudentCGPA);
+// const profile = {
+//     username: "jon12",
+//     fullName: "Jone Don",
+//     Bio: "I am a Developer.",
+//     Posts:33,
+//     follower:199,   
+//     following:5,   
+// };
 // arithmtic operator
-let s = 10;
-let b= 20;
-console.log(s+b);
-console.log(s%b);
-console.log(s-b);
-console.log(s*b);
-console.log(s/b);
-console.log(s ** b);
-console.log(s**s);
+// let s = 10;
+// let b= 20;
+// console.log(s+b);
+// console.log(s%b);
+// console.log(s-b);
+// console.log(s*b);
+// console.log(s/b);
+// console.log(s ** b);
+// console.log(s**s);
 // assinment operator
 // let s = 10;
 // let b= 20;
-s+=2;
+// s+=2;
 // s-=2;
 // s*=2;
 // s/=2;
 // s%=2;
-console.log(s)
+// console.log(s)
 // comparison operator
 // (equal to == )(not equal to !=)(grater than >)
 // (less than <)(grater than equal to >=)
 // (less than equal to <=)  
+// logical oprator 
+// && (and oprator )
+// let cond1 = (s < b);
+// let cond2 = (a === b);
+// console.log(cond1 && cond2 );
+// || (or operator)
+// let s = 5;
+// let b = 6;
+// let a = 10;
+// let cond1 = (s < b);//true
+// let cond2 = (a == 9);//true
+// console.log(cond1 || cond2 );
+let age = 45 ;
+if (age > 10 && age <15 ){
+    console.log("you can play with chilldren");
+}
+if(age>=16 && age <= 20 ){
+    console.log("you can play with us!");
+}
+else{
+    console.log("you can play with internationl Team");
+}
+let mood="dark";
+let color;
+if(mood==="dark"){
+    color= "black";
+}
+else{
+    color="white"
+}
+console.log(color);
+let num = 40;
+if (num%2 == 1){
+    console.log("your number is odd")
+}else{
+    console.log("your number is even")
+}
+let buget = 120000;
+let buy_Bike;
+if(buget>= 60000 && buget<= 70000){
+    console.log("showing the 2nd hand 70 bike");
+}else if(buget>= 70000 && buget<= 100000){
+    console.log("showing the 2nd hand 125 bike");
+}else{
+    console.log("showing the New bikes");
+}
