63b87ecc3cbc8cdc9ff1e7f6672d7f217744f414
rehmantariq0 <chaudharyrehman4343@gmail.com>
2025-03-04 20:52:34 +0500

    update

--- firstwebdevelop.js
+++ firstwebdevelop.js
@@ -111,3 +111,31 @@ if(buget>= 60000 && buget<= 70000){
 }else{
     console.log("showing the New bikes");
 }
+// ternary operator
+// condition ? ture output : false output
+let res = age > 18 ? "adult" : "Not adult";
+console.log(res);
+// switch statement
+let expr = prompt("Enter a Fruit Name:");
+switch (expr) {
+    case "Oranges":
+      console.log("Oranges are $0.59 a pound.");
+      break;
+    case "Apples":
+      console.log("Apples are $0.32 a pound.");
+      break;
+    case "Bananas":
+      console.log("Bananas are $0.48 a pound.");
+      break;
+    case "Cherries":
+      console.log("Cherries are $3.00 a pound.");
+      break;
+    case "Mangoes":
+    case "Papayas":
+      console.log("Mangoes and papayas are $2.79 a pound.");
+      break;
+    default:
+      console.log(`Sorry, we are out of ${expr}.`);
+  }
+  console.log("Is there anything else you'd like?");
+  
